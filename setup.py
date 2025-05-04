4079fcf7647a71958dea07404552a993fa5174ea
Marszpace <iamgmgm@gmail.com>
2025-05-05 00:19:25 +0700

    Changes to be committed: 	new file:   scoretypes/GroupMinPrereqDisplayAll.py 	modified:   setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name="GroupMinPrereq",
-    version="1.0",
+    version="2.0",
     packages=find_packages(),
     entry_points={
         "cms.grading.scoretypes": [
             "GroupMinPrereq=scoretypes.GroupMinPrereq:GroupMinPrereq"
+            "GroupMinPrereqDisplayAll=scoretypes.GroupMinPrereqDisplayAll:GroupMinPrereqDisplayAll"
         ]
     }
 )
