9fa0bb32bd08de278c63c7fcba6eb3b8c034f2fa
Marszpace <iamgmgm@gmail.com>
2025-05-05 09:47:09 +0700

    changed version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="GroupMinPrereq",
-    version="2.0",
+    version="2.1",
     packages=find_packages(),
     entry_points={
         "cms.grading.scoretypes": [
